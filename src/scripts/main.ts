36e51fd210328f48f9ced70e74118e117fd16f63
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-05-29 14:49:50 +0300

    front-hero

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,13 @@
 import "../styles/main.scss";
 import Header from "./components/header";
+import Scroll from "./scroll";
+import BackTopButton from "./components/back-top-button";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Header();
+  new BackTopButton(900, "top");
+  const scroll = new Scroll();
+
+  scroll.initSmoothScroll();
+  scroll.initAOS();
 });
